8cd294c25e5c710601872eee1fdb1f26b1c6e013
Graeme <graemenrowland@gmail.com>
2019-08-13 09:53:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstName = "Graeme"
+var surName = "Rowland"
+console.log(firstName, surName);
+var vorName = "Frank";
+var nachName = "Sidebottom";
+var job = "Entertainer";
+console.log(vorName, nachName, job);
