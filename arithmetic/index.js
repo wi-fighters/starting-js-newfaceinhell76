4f2f04a6bc253e88d9c0924eed0e3afa85291bb6
Graeme <graemenrowland@gmail.com>
2019-08-13 14:25:27 +0200

    second commit

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -1,4 +1,10 @@
-var x = 17 / 30 * 100
-console.log (x)
+const x = 17 / 30 * 100
+console.log (`result is ${x}%`);
+
+const y = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4
+const z = 10
+console.log (`average is ${y/z} per hour`)
+
+
 
 
