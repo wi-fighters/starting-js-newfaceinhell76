9529f135653463f0ecd3ec1707df47bd5e405336
Graeme <graemenrowland@gmail.com>
2019-08-13 10:37:08 +0200

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var firstName = "Graeme"
 var surName = "Rowland"
-console.log(firstName, surName);
+//console.log(firstName, surName);
 var vorName = "Frank";
 var nachName = "Sidebottom";
 var job = "Entertainer";
-console.log(vorName, nachName, job);
+//console.log(vorName, nachName, job);
+
+var age = "45";
+var height = "176";
+
+console.log(vorName, nachName, ", a " + age + " year old", job, "is " + height + "cm tall");
+
+let length = 16.78; 
+let lastName = "Johnson"; 
+let car = ""; 
+let isOpen = true;
+
+//use let command, not var//
+
